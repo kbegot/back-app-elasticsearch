f2762c5fb5fa408b0f7874161870e2bcfb8c35d7
Kévin Begot <begot.kevin@gmail.com>
2024-05-13 17:17:55 +0200

    [UPDATE] elasticsearch module

--- src/elasticsearch/elasticsearch.module.ts
+++ src/elasticsearch/elasticsearch.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ElasticsearchService } from '@nestjs/elasticsearch'; // Importez ElasticsearchService depuis @nestjs/elasticsearch
 import { MyElasticsearchService } from './elasticsearch.service';
 import { ElasticsearchController } from './elasticsearch.controller';
 
 @Module({
-  providers: [MyElasticsearchService],
+  providers: [
+    MyElasticsearchService,
+    {
+      provide: ElasticsearchService,
+      useFactory: () =>
+        new ElasticsearchService({
+          node: 'https://536d7c1013ee44b4a249d08e8e22d733.us-central1.gcp.cloud.es.io',
+          auth: {
+            apiKey:
+              'ODBHRGNvOEJqVllJLTZDOVhTN3I6MGRqRGhsbTFRU0dvLU9fTGVQLVFKUQ==', // Utilisez votre clé API pour l'authentification
+          },
+        }),
+    },
+  ],
   controllers: [ElasticsearchController],
 })
 export class ElasticsearchModule {}
--- src/elasticsearch/elasticsearch.service.ts
+++ src/elasticsearch/elasticsearch.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { ElasticsearchService as NestElasticsearchService } from '@nestjs/elasticsearch';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
 
 @Injectable()
 export class MyElasticsearchService {
-  constructor(private readonly esService: NestElasticsearchService) {}
+  constructor(private readonly esService: ElasticsearchService) {}
 
   async indexDocument(index: string, document: any): Promise<any> {
     return await this.esService.index({
