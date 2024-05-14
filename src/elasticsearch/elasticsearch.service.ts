de75c37722e418d22fd8a12319d54b485c247415
Julien BERTRAND <j.bertrand.sin@gmail.com>
2024-05-14 10:52:27 +0200

    test commit access

--- src/elasticsearch/elasticsearch.service.ts
+++ src/elasticsearch/elasticsearch.service.ts
@@ -14,7 +14,7 @@ export class MyElasticsearchService {
     try {
       // Check if the index already exists
       const indexExists = await this.esService.indices.exists({ index });
-
+console.log(indexExists)
       if (indexExists) {
         // If the index already exists, throw an exception with an error code 409 (Conflict)
         throw new HttpException(
